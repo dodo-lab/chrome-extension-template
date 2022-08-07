a05cc256496bf49fa2b34eb1135cb6b4129333e8
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-08-07 09:12:39 +0900

    🔧: build config

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/main.ts'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
+  },
+});
